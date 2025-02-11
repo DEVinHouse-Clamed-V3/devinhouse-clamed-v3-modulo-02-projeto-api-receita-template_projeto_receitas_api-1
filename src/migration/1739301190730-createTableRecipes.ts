07746f7c016380e8d1ca73d3cd147ee473e9a6ed
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-02-11 16:24:49 -0300

    create migration for recipes table with necessary columns

--- src/migration/1739301190730-createTableRecipes.ts
+++ src/migration/1739301190730-createTableRecipes.ts
@@ -1,11 +1,50 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateTableRecipes1739301190730 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'recipes',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                    length: '200',
+                    isNullable: false
+                },
+                {
+                    name: 'preparation_time',
+                    type: 'time',
+                    isNullable: false
+                },
+                {
+                    name: 'is_fitness',
+                    type: 'boolean',
+                    default: false
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'now()'
+                },
+                {
+                    name: 'updated_at',
+                    type: 'timestamp',
+                    default: 'now()'
+                }
+            ]
+        }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('recipes');
     }
 
 }
