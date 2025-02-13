6d93281b1fa60b30c00a6706246be5387962efc1
Lucas da Silva <sklucassilva@gmail.com>
2025-02-13 18:00:54 -0300

    feat: implementa migration query

---
+++ src/migration/1739480303589-RecipesTable.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class RecipesTable1739480303589 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'recipes',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '200',
+            isNullable: false,
+          },
+          {
+            name: 'preparation_time',
+            type: 'time',
+            isNullable: false,
+          },
+          {
+            name: 'is_fitness',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('recipes');
+  }
+}
