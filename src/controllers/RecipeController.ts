779ba2c9e52d0cb872a8580db480c429c6b0fa60
Davi-Zimmer <davizimmerv5@gmail.com>
2025-02-14 19:25:16 -0300

    entidade da Recipe

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -1,6 +1,26 @@
+import { Collection, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity()
 class RecipeController {
- 
+    
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    preparation_time: Date
+
+    @Column()
+    is_fitness: boolean
+
+    @Column()
+    created_at: Date
+
+    @Column()
+    updated_at: Date
+
 }
 
 export default RecipeController;
---
+++ src/database/migrations/1739571153812-RecipesTable.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class RecipesTable1739571153812 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        /*
+        queryRunner.createTable(
+            new Table({
+                    name: 'recipes',
+                    columns: [
+                        {
+                            name: 'id',
+                            isPrimary: true,
+                            type: 'serial'
+                        },
+
+                        {
+                            name: 'name',
+                            type: 'varchar',
+                            isNullable: false,
+                            length: '200',
+                        },
+
+                        {
+                            name: 'preparation_time',
+                            type: 'time',
+                            isNullable: false,
+                        },
+
+                        {
+                            name: 'is_fitness',
+                            type: 'boolean',
+                            default: false
+                        },
+
+                        {
+                            name: 'created_at',
+                            type: 'date'
+                        },
+                        
+                        {
+                            name: 'updated_at',
+                            type: 'date'
+                        }
+                    ]
+            })
+        )
+        */
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
